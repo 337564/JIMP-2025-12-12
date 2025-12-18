019d53d1eb3cd05cb270704fa8e899a023e9a204
opivanyk <01200569@pw.edu.pl>
2025-12-19 00:32:31 +0100

    Implementacja eliminacji Gaussa. Modyfikacja pliku main.c w celu wyswietlenia wyniku eliminacji. Implementacja nowych plikow testowych A1, b1

--- src/gauss.c
+++ src/gauss.c
@@ -1,14 +1,33 @@
 #include "gauss.h"
+#include <stdio.h>
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
+    /*
+  	Tutaj nalezy umiescic wlasciwa implemntacje.
+	*/
 
-		return 0;
+	for(int i=0; i<mat->r; i++){
+		if(mat->data[i][i]==0.0){
+			fprintf(stderr,"!!! Blad: macierz osobliwa - dzielenie przez 0.\n");
+			return 1;
+		}
+		for(int j=i+1; j<mat->r; j++){
+			double w = mat->data[j][i]/mat->data[i][i];
+			for(int k=i; k<=mat->c; k++){
+				if (k==mat->c){
+					b->data[j][0] = b->data[j][0] - w*b->data[i][0];
+				} else {
+					mat->data[j][k] = mat->data[j][k] - w*mat->data[i][k];
+				}
+				
+			}
+		}
+	}
+
+
+	return 0;
 }
-
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 
 
 int main(int argc, char ** argv) {
-	int res; 
+	int res;
 	Matrix * A = readFromFile(argv[1]);
 	Matrix * b = readFromFile(argv[2]);
 	Matrix * x;
@@ -17,6 +17,9 @@ int main(int argc, char ** argv) {
 	printToScreen(b);
 
 	res = eliminate(A,b);
+	printf("Schodkowanie:\n");
+	printToScreen(A);
+	
 	x = createMatrix(b->r, 1);
 	if (x != NULL) {
 		res = backsubst(x,A,b);
@@ -24,7 +27,7 @@ int main(int argc, char ** argv) {
 		printToScreen(x);
 	  freeMatrix(x);
 	} else {
-					fprintf(stderr,"Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
+					fprintf(stderr,"Blad! Nie moglem utworzyc wektora wynikowego x.\n");
 	}
 
 	freeMatrix(A);
