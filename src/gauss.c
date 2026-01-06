33473a4730224f32f27b1c9ca9873f2f5d11532c
opivanyk <01200569@pw.edu.pl>
2026-01-06 17:39:45 +0100

    Rozszerzenie metody eleminacji Gaussa 1

--- src/gauss.c
+++ src/gauss.c
@@ -1,20 +1,34 @@
 #include "gauss.h"
 #include <stdio.h>
+#include <math.h> // korzystanie z funk. fabs()
 
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
 int eliminate(Matrix *mat, Matrix *b){
-    /*
-  	Tutaj nalezy umiescic wlasciwa implemntacje.
-	*/
-
+	
 	for(int i=0; i<mat->r; i++){
-		if(mat->data[i][i]==0.0){
-			fprintf(stderr,"!!! Blad: macierz osobliwa - dzielenie przez 0.\n");
-			return 1;
+		
+		// Wybor najwiekszego elementu z kolumny
+		int maxi= i;
+		for(int j=i+1; j<mat->r; j++){
+			if(fabs(mat->data[j][i]) > fabs(mat->data[maxi][i])){
+				maxi = j;
+			}
 		}
+		if(mat->data[maxi][i]==0.0){ // jesli wszystkie elementy w kolumnie to 0, pomin te kolumne
+			continue;
+		}
+		// Zamiana wierszy
+		double * copy = mat->data[i];
+		double * copyb = b->data[i];
+		mat->data[i] = mat->data[maxi];
+		b->data[i] = b->data[maxi];
+		mat->data[maxi] = copy;
+		b->data[maxi] = copyb;
+		// Eliminacja Gaussa
+		// ...
 		for(int j=i+1; j<mat->r; j++){
 			double w = mat->data[j][i]/mat->data[i][i];
 			for(int k=i; k<=mat->c; k++){
@@ -31,3 +45,4 @@ int eliminate(Matrix *mat, Matrix *b){
 
 	return 0;
 }
+
